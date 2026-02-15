909437a70a356f750e166e566f81565695ca4c2b
Cheng <chengcheng@miao.ms>
2026-02-16 01:28:19 +0800

    fix(ui/image): guard image.nvim setup in headless sessions

--- config/ui/image.nix
+++ config/ui/image.nix
@@ -2,6 +2,16 @@
   plugins.image = {
     enable = true;
 
+    luaConfig.pre = /* lua */ ''
+      if #vim.api.nvim_list_uis() == 0 then
+        package.preload['image'] = function()
+          return {
+            setup = function() end,
+          }
+        end
+      end
+    '';
+
     settings = {
       backend = "kitty";
     };
